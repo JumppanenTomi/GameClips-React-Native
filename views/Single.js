b67a8978c6302ca90cc1b6a79d9c5e0770da8727
jumppanentomi <tomi.jumppanen@hotmail.com>
2023-02-19 21:27:56 +0200

    actually works now. Commenting is still WIP

--- views/Single.js
+++ views/Single.js
@@ -3,35 +3,101 @@ import {uploadsUrl} from '../utils/variables';
 import PropTypes from 'prop-types';
 import {Video } from "expo-av";
 import Text from "../components/Text";
-import {Image, SafeAreaView, StyleSheet, TouchableOpacity, View} from "react-native";
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import shareClip from "../components/functions/shareClip";
 import profile from "../components/functions/profile";
+import {useComments} from "../hooks/ApiHooks";
+import FormInput from "../components/FormInput";
+import {useForm} from "react-hook-form";
 
-const Single = ({route}) => {
+const Single = ({route, navigation}) => {
   const [avatar, setAvatar] = useState('');
   const [owner, setOwner] = useState('');
+  const [comments, setComments] = useState([]);
   const [status, setStatus] = useState({});
-
-  const {title, description, filename, time_added: timeAdded, user_id: userId,} = route.params;
+  const [isHidden, toggleHidden] = useState(true)
+  const {title, description, filename, user_id: userId,} = route.params;
   const video = useRef(null);
+  const {
+    control,
+  } = useForm({
+    defaultValues: {username: '', password: ''},
+  });
 
   const getData = async ()=>{
     setAvatar(await profile().loadAvatar(userId));
     setOwner(await profile().loadOwner(userId));
+    setComments(await useComments().getCommentsById(route.params.file_id));
   }
-
   useEffect( () => {
+    console.log("data received")
     getData();
   }, []);
 
   const handleVideoPress = () => {
-    if (status.isPlaying) {
+    if (status.isPlaying && isHidden) {
       video.current.pauseAsync();
-    } else {
+    } else if (isHidden) {
       video.current.playAsync();
+    } else{
+      Keyboard.dismiss()
+      toggleHidden(true)
     }
   }
+
+  const commentStyles = StyleSheet.create({
+    heading:{
+      marginTop: 32,
+      marginBottom: 18,
+      fontSize: 18,
+      color: '#fff'
+    },
+    scrollContainer:{
+      width: "100%",
+      minHeight: 300,
+    },
+    comment:{
+      color: "#fff",
+      fontSize: 16
+    },
+    poster:{
+      fontWeight: "700",
+      fontSize: 16,
+      color: "rgba(255,255,255,0.51)",
+    },
+    text:{
+      width: "100%",
+      flexWrap: "wrap",
+      marginBottom: 20,
+    },
+    commentContainer: {
+      paddingLeft: 24,
+      paddingRight: 24,
+      paddingBottom: 10,
+      position: "absolute",
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      display: isHidden ? 'none' : 'flex',
+      zIndex: 99,
+      maxHeight: "60%",
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: "#25253B",
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+  })
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#000000'}}>
@@ -50,17 +116,47 @@ const Single = ({route}) => {
           onPlaybackStatusUpdate={status => setStatus(() => status)}
         />
       </TouchableOpacity>
-        <View style={styles.controlContainer}>
-          <Image style={styles.tinyProfileImage} source={{uri: uploadsUrl + avatar}}/>
-          <Ionicons style={styles.icon} onPress={() => {console.log("liked")}} name="heart" size={40} color="#ffffff" />
-            <Ionicons style={styles.icon} onPress={() => {console.log("commented")}} name="chatbubble-ellipses" size={40} color="#ffffff" />
+      <Ionicons style={styles.back} onPress={() => {navigation.goBack()}} name="chevron-back-outline" size={40} color="#ffffff" />
+      <View style={styles.controlContainer}>
+            <Image style={styles.tinyProfileImage} source={{uri: uploadsUrl + avatar}}/>
+            <Ionicons style={styles.icon} onPress={() => {console.log("liked")}} name="heart" size={40} color="#ffffff" />
+            <Ionicons style={styles.icon} onPress={() => {toggleHidden(false)}} name="chatbubble-ellipses" size={40} color="#ffffff" />
             <Ionicons style={styles.icon} onPress={() => {shareClip().onShare(uploadsUrl+filename)}} name="share-social" size={40} color="#ffffff" />
         </View>
-        <View style={styles.infoContainer}>
+      <View style={styles.infoContainer}>
           <Text type="brightSubHeading" style={{fontSize: 16, fontWeight: '700'}}>@{owner}</Text>
           <Text type="brightSubHeading" style={{fontSize: 18,}}>{title}</Text>
           <Text style={{color: '#fff'}}>{description}</Text>
         </View>
+      <View style={{paddingLeft: 24, position: "absolute", display: isHidden ? 'none' : 'flex', height: "10%", bottom: 0, left: 0, right: 0, backgroundColor: "#25253B",}}>
+        <Text></Text>
+      </View>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : 'height'}>
+      <View style={commentStyles.commentContainer}>
+        <Text style={commentStyles.heading}>Comments</Text>
+        <ScrollView style={commentStyles.scrollContainer}>
+          {comments.reverse().map((item) => (
+            <View style={commentStyles.text} key={item.comment_id}>
+              <Text style={commentStyles.comment}>
+                <Text style={commentStyles.poster}>{item.user_id}</Text> {item.comment}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View style={{width: "100%", flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+          <FormInput
+            name="Post a comment"
+            label="Post a comment"
+            control={control}
+            style={{flex: 10, marginTop: 16, marginBottom: 16,}}
+          />
+          <View style={{borderRadius: 60,backgroundColor: "#8C8AFA", width: 46, padding: 13, marginLeft: 8, marginTop: 16, marginBottom: 16, }}>
+            <Ionicons onPress={() => {shareClip().onShare(uploadsUrl+filename)}} name="send" size={20} color="#ffffff" />
+          </View>
+        </View>
+      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -68,7 +164,6 @@ const Single = ({route}) => {
 Single.propTypes = {
   route: PropTypes.object,
 };
-
 
 const styles = StyleSheet.create({
   videoContainer: {
@@ -96,6 +191,14 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginBottom: 15,
     marginRight: 10,
+  },
+  back:{
+    position: "absolute",
+    marginTop: 40,
+    marginBottom: 15,
+    marginLeft: 10,
+    top:0,
+    left:0
   },
   tinyProfileImage:{
     borderRadius: 50,
